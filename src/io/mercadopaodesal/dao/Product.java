284539884394c71eb6a2e4fae4a385d14708c3fa
let-lucasOliveira <lucas.nro1000@gmail.com>
2025-06-28 11:35:09 -0300

    :memo: Add Product and Suplier class.

---
+++ src/io/mercadopaodesal/dao/Product.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package io.mercadopaodesal.dao;
+
+/**
+ *
+ * @author lucas
+ */
+public class Product {
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getCatId() {
+        return catId;
+    }
+
+    public void setCatId(int catId) {
+        this.catId = catId;
+    }
+
+    public String getCodBarras() {
+        return codBarras;
+    }
+
+    public void setCodBarras(String codBarras) {
+        this.codBarras = codBarras;
+    }
+    private int id;
+    private String nome;
+    private double preco;
+    private int catId;
+    private String codBarras;
+}
